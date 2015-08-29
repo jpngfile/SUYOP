417abb7c04230c3e41e9d8c7e268416b7b540941
jpngfile <jsjpng@gmail.com>
2015-08-29 01:54:34 -0400

    MOAR CONTENT + filtering.
    Just added 11 more extracurriculars to the portfolio. Oh yeah, and added
    the isotope plug-in so there is content filtering.

--- js/freelancer.js
+++ js/freelancer.js
@@ -15,6 +15,36 @@ $(function() {
     });
 });
 
+//Portfolio
+
+
+$( document ).ready( function() {
+  // init Isotope
+  var $container = $('.mosaic').isotope({
+    itemSelector: '.mosaic-item',
+    layoutMode: 'masonry',
+  });
+
+  // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    //filterValue = filterFns[ filterValue ] || filterValue;
+    $container.isotope({ filter: filterValue });
+  });
+
+
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
+});
+		
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
