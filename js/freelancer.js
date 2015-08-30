a4e9b37e56dc3ae838bee9ca43372ed0f9c950c4
jpngfile <jsjpng@gmail.com>
2015-08-30 19:58:13 -0400

    Gahh. Reorganized filters. Overlapping problem

--- js/freelancer.js
+++ js/freelancer.js
@@ -53,6 +53,11 @@ $( document ).ready( function() {
   
 });
 
+$(".dropdown-menu li button").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});
+	
 //helper function
 function concatValues (obj) {
 var value = '';
